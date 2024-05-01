14213e32e3d57787b0fb2874b3e55b4b49274cb4
tankeu10 <tankeucynthia603@gmail.com>
2024-05-01 21:43:00 +0200

    first commit

---
+++ src/components/authentification.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import stockageLogo from '../assets/stockageLogo.jpg';
+
+function authentification() {
+  return (
+    <div className='authentification'>
+        <div className='leftSide'>
+            <img style={{height:'100%'}} src={stockageLogo} alt=''/>
+        </div>
+        <div className= 'rightSide'>
+            lorem ftfhghggffdfyghghcfdhfyghgyg</div>
+      
+    </div>
+  )
+}
+
+export default authentification
